66e8806b52c47372e133101b796f0b8fe1a52093
Facundo Vallejos <Facund097dv@gmail.com>
2023-12-20 00:31:27 -0300

    agrega calculos y imprime resultados

--- task-2/task-2.js
+++ task-2/task-2.js
@@ -1,9 +1,6 @@
 /*
 TAREA:
 Crear una interfaz que permita agregar ó quitar (botones agregar y quitar) inputs+labels para completar el salario anual de cada integrante de la familia que trabaje.
-Al hacer click en "calcular", mostrar en un elemento pre-existente el mayor salario anual, menor salario anual, salario anual promedio y salario mensual promedio.
-
-Punto bonus: si hay inputs vacíos, ignorarlos en el cálculo (no contarlos como 0).
 */
 
 let counterFamily = 0;
@@ -20,11 +17,11 @@ function createElementsFamily() {
 
     let $label = document.createElement("label");
     $label.className = "element-family";
-    $label.textContent = `Familiar #${counterFamily + 1}`;
+    $label.textContent = `Salario mensual #${counterFamily + 1}`;
 
     let $input = document.createElement("input");
     $input.type = "number";
-    $input.className = "element-family annual-salary";
+    $input.className = "element-family monthly-salary";
     return [$br, $label, $input];
 }
 
@@ -50,10 +47,95 @@ document.querySelector("#remove-family").onclick = function (event) {
     let $elementsFamily = getElementsFamily();
 
     if ($elementsFamily.length !== 0) {
-
         removeElementsFamily($elementsFamily);
         counterFamily--;
     }
 
     event.preventDefault();
 };
+
+// calculos
+/**
+Al hacer click en "calcular", mostrar en un elemento pre-existente el mayor salario anual, menor salario anual, salario anual promedio y salario mensual promedio.
+Punto bonus: si hay inputs vacíos, ignorarlos en el cálculo (no contarlos como 0).
+ */
+document.querySelector("#calculate-salary").onclick = function (event) {
+
+    let monthlySalary = getMonthlySalary();
+    if (monthlySalary.length !== 0) {
+        let highterAnnualSalary = calculateHighterAnnualSalary(monthlySalary);
+        let lowestAnnualSalary = calculateLowestAnnualSalary(monthlySalary);
+        let averageAnnualSalary = calculateAverageAnnualSalary(monthlySalary);
+        let averageMonthlySalary =  calculateAverageMonthlySalary(monthlySalary);
+        printResults(highterAnnualSalary,lowestAnnualSalary,averageAnnualSalary,averageMonthlySalary);
+    }
+   
+
+    event.preventDefault();
+};
+
+function getMonthlySalary() {
+    const monthlySalary = [];
+    let $monthlySalary = document.querySelectorAll(".monthly-salary");
+
+    for (let i = 0; i < $monthlySalary.length; i++) {
+        if (Number($monthlySalary[i].value) !== 0) {
+            monthlySalary.push(Number($monthlySalary[i].value));
+        }
+    }
+    return monthlySalary;
+}
+
+function calculateHighterAnnualSalary(monthlySalary) {
+    const MONTHS_OF_YEAR = 12;
+    let highter = monthlySalary[0];
+
+    for (let i = 0; i < monthlySalary.length; i++) {
+        highter = highter > monthlySalary[i] ? highter : monthlySalary[i];
+    }
+    return highter * MONTHS_OF_YEAR;
+}
+
+function calculateLowestAnnualSalary(monthlySalary) {
+    const MONTHS_OF_YEAR = 12;
+    let lowest = monthlySalary[0];
+
+    for (let i = 0; i < monthlySalary.length; i++) {
+        lowest = lowest < monthlySalary[i] ? lowest : monthlySalary[i];
+    }
+    return lowest * MONTHS_OF_YEAR;
+}
+
+function calculateAverageAnnualSalary(monthlySalary) {
+    const MONTHS_OF_YEAR = 12;
+    let accumulator = 0;
+
+    for (let i = 0; i < monthlySalary.length; i++) {
+
+        accumulator += monthlySalary[i];
+
+    }
+
+    return (accumulator / monthlySalary.length) * MONTHS_OF_YEAR;
+
+}
+
+function calculateAverageMonthlySalary(monthlySalary){
+    let accumulator = 0;
+
+    for (let i = 0; i < monthlySalary.length; i++) {
+
+        accumulator += monthlySalary[i];
+
+    }
+    return accumulator / monthlySalary.length;
+}
+
+function printResults(highterAnnualSalary,lowestAnnualSalary,averageAnnualSalary,averageMonthlySalary) {
+
+    document.querySelector("#highter-annual-salary").textContent = `El salario anual mas alto: ${highterAnnualSalary}, `;
+    document.querySelector("#lowest-annual-salary").textContent = `El salario anual mas bajo: ${lowestAnnualSalary}, `;
+    document.querySelector("#average-annual-salary").textContent = `El salario anual promedio: ${averageAnnualSalary.toFixed(2)}, `;
+    document.querySelector("#average-monthly-salary").textContent = `El salario mensual promedio: ${averageMonthlySalary.toFixed(2)} `;
+
+}
